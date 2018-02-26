07da733ec1210d54d84bdd3161b9d5d719863ea8
brslv <brslvg@gmail.com>
2018-02-26 07:09:53 +0200

    Add basic navigation styles

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import './Navigation.css'
+import { Link } from 'react-router-dom'
+import Button from '../../atoms/Button'
 
-const Navigation = ({ onLogout }) => {
-  return (
-    <div className="Component--Navigation">
-      <ul>
-        <li><button onClick={onLogout}>Logout</button></li>
-        <li>Home</li>
-        <li>Test</li>
-      </ul>
-    </div>
-  )
+class Navigation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hidden: true
+    }
+  }
+
+  show() {
+    this.setState({ hidden: false })
+  }
+
+  hide() {
+    this.setState({ hidden: true })
+  }
+
+  render() {
+    let className = `Component--Navigation`
+    if (this.state.hidden) {
+      className += ' hidden'
+    }
+
+    return (
+      <div
+        className={className}
+        onMouseLeave={this.hide.bind(this)}
+      >
+        <div
+          className="hamburger"
+          onClick={this.show.bind(this)}
+          onMouseEnter={this.show.bind(this)}
+        >
+          ☰
+        </div>
+        <ul className="list">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/books">Books</Link></li>
+        </ul>
+        <Button className="logout-button stroke-only small" onClick={this.props.onLogout}>Logout</Button>
+      </div>
+    )
+  }
 }
 
 export default withRouter(Navigation)
