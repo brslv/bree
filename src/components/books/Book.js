3f20b3ddec7620d0f30175ef5543063edd1d2d04
brslv <brslvg@gmail.com>
2018-03-01 07:35:29 +0200

    Add delete button to each book in the list

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from '../../atoms/Card'
 import Button from '../../atoms/Button'
 import Badge from '../../atoms/Badge'
 import { Link } from 'react-router-dom'
 import './Book.css'
 
-const Book = ({ id, title, description }) => {
-  return (
-    <Card className="Component--Book">
-      <h3 className="title">{title}</h3>
-      <div className="content">{description}</div>
-      <div className="footer">
-        <div className="left">
-          <Badge text="Draft" />
+class Book extends Component {
+  onDelete(e) {
+    console.warn('deleting book...displaying confirmation modal')
+  }
+
+  render() {
+    const {
+      id,
+      title,
+      description
+    } = this.props
+
+    return (
+      <Card className="Component--Book">
+        <div className="header">
+          <h3 className="title">{title}</h3>
+          <span className="delete-button" onClick={this.onDelete.bind(this)}>&times;</span>
         </div>
-        <div className="right">
-          <Link to={`/books/${id}/edit/`}>
-            <Button className="small stroke-only">Edit</Button>
-          </Link> &nbsp;
-          <Button className="small">Write</Button>
+        <div className="content">{description}</div>
+        <div className="footer">
+          <div className="left">
+            <Badge text="Draft" />
+          </div>
+          <div className="right">
+            <Link to={`/books/${id}/edit/`}>
+              <Button className="small stroke-only">Edit</Button>
+            </Link> &nbsp;
+            <Button className="small">Write</Button>
+          </div>
         </div>
-      </div>
-    </Card>
-  )
+      </Card>
+    )
+  }
 }
 
 export default Book
