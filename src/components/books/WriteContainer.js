c1841049f654122c788a6cf2ed0a5c7648b7df05
brslv <brslvg@gmail.com>
2018-03-02 11:10:17 +0200

    Move the libs folder from src/ to public/ and load all the static libs from the index.html file

--- src/components/books/WriteContainer.js
+++ src/components/books/WriteContainer.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
-import SimpleMDE from '../../libs/simplemde/src/js/simplemde'
-import '../../libs/codemirror/codemirror.css'
-import '../../libs/simplemde/src/css/simplemde.css'
 import './WriteContainer.css'
 
 class WriteContainer extends Component {
   componentDidMount() {
-    this.simpleMDE = new SimpleMDE({ element: document.querySelector('.test') })
+    this.simpleMDE = new window.SimpleMDE({ element: document.querySelector('.test') })
   }
 
   shouldComponentUpdate() {
