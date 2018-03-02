f92134c3fece428eed2ee26c67e8b83a970fe233
brslv <brslvg@gmail.com>
2018-03-02 10:15:15 +0200

    Add editor in books Write page

--- src/components/books/WriteContainer.js
+++ src/components/books/WriteContainer.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
+import SimpleMDE from '../../libs/simplemde/src/js/simplemde'
+import '../../libs/codemirror/codemirror.css'
+import '../../libs/simplemde/src/css/simplemde.css'
 import './WriteContainer.css'
 
 class WriteContainer extends Component {
+  componentDidMount() {
+    this.simpleMDE = new SimpleMDE({ element: document.querySelector('.test') })
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
   render() {
     return (
       <div className="Component--WriteContainer">
@@ -10,11 +21,7 @@ class WriteContainer extends Component {
         </div>
 
         <div className="right">
-          Content
-        </div>
-
-        <div className="right">
-          Parsed content...
+          <textarea className="test" ref="textarea" />
         </div>
       </div>
     )
