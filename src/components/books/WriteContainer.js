37e84e53242e3c1045742721ff5a4432abcf6253
brslv <brslvg@gmail.com>
2018-03-05 18:29:29 +0200

    Style the chapters context menu

--- src/components/books/WriteContainer.js
+++ src/components/books/WriteContainer.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import Button from '../../atoms/Button'
 import './WriteContainer.css'
 
 class WriteContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      chaptersListIsHidden: true
+    }
+  }
+
   componentDidMount() {
     this.simpleMDE = new window.SimpleMDE({ element: document.querySelector('.chapter-content-editor') })
+  }
+
+  openChaptersList(e) {
+    this.setState((prevState) => ({ chaptersListIsHidden: !prevState.chaptersListIsHidden }))
   }
 
   render() {
     return (
       <div className="Component--WriteContainer">
-        <div className="left">
-          <h2>Chapters</h2>
-          <ol className="chapters-list">
-            <li className="chapter"><Link to="/">Chapter 1 - Intro</Link></li>
-            <li className="chapter"><Link to="/">Chapter 1 - Something</Link></li>
-            <li className="chapter"><Link to="/">Chapter 1 - Something more!</Link></li>
-          </ol>
-        </div>
+        <textarea className="chapter-content-editor" ref="textarea" />
+        <div className="chapters-context-menu">
+          <div ref="chaptersList" className={`chapters-list ${this.state.chaptersListIsHidden ? 'hidden' : ''}`}>
+            <div className="chapter">
+              <div className="title">This is the first chapter...</div>
+              <div className="controls">
+                <Button className="small stroke-only">Edit</Button>
+                <Button className="small">Write</Button>
+              </div>
+            </div>
 
-        <div className="right">
-          <textarea className="chapter-content-editor" ref="textarea" />
+            <div className="chapter">
+              <div className="title">This is the first chapter...</div>
+              <div className="controls">
+                <Button className="small stroke-only">Edit</Button>
+                <Button className="small">Write</Button>
+              </div>
+            </div>
+
+            <div className="chapter">
+              <div className="title">This is the first chapter and this is my second chapter here...it's a long long chapter, you know....</div>
+              <div className="controls">
+                <Button className="small stroke-only">Edit</Button>
+                <Button className="small">Write</Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="chapters-list-button" onClick={this.openChaptersList.bind(this)}>
+            <span>☷</span>
+          </div>
         </div>
       </div>
     )
